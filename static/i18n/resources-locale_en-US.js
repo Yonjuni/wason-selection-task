6f4cc6396b868993edf91bba80122642e4196a42
Karola Marky <karola@pinyto.de>
2014-10-23 13:58:34 +0900

    task structure changed

--- static/i18n/resources-locale_en-US.js
+++ static/i18n/resources-locale_en-US.js
@@ -2,11 +2,11 @@
     {
         "key"        : "_nextButton_",
         "value"      : "Next task",
-        "description": ""
+        "description": "Next button used during the tasks"
     },
-     {
+    {
         "key"        : "_submitButton_",
         "value"      : "Submit results",
-        "description": ""
+        "description": "Submit button used after the tasks to submit results"
     }
 ]
