739ff17ee51c78ffb871dd80b64e5604597e6794
Karola Marky <karola@pinyto.de>
2014-10-23 12:37:26 +0900

    App activated in settings.py

--- wason_selection_task/settings.py
+++ wason_selection_task/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'selectionTask',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,7 +66,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'jp-JP'
 
 TIME_ZONE = 'UTC'
 
