7cd7f76e940f91c3763e7ee8737c9cb65cb44bbc
Karola Marky <karola@pinyto.de>
2014-10-23 12:34:31 +0900

    Angular structure added, project folder cleaned up

---
+++ static/js/controller/questionCtrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+trueOrFalseApp.controller("questionCtrl", function ($scope) {
+
+    $scope.clickButton = function (answer) {
+
+    };
+
+    $scope.dontKnow = function () {
+
+    };
+
+});
