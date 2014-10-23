aeb71bf176c5db0a12e56eb8274b37ad49ea5502
Karola Marky <karola@pinyto.de>
2014-10-23 12:16:20 +0900

    old projects files cleaned up

--- static/js/controller/introductionCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-trueOrFalseApp.controller("introductionCtrl", function ($scope) {
-
-
-
-});
--- static/js/service/service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('BackendService', [])
-    .factory('Backend', function ($http) {
-        return {
-            loadQuestions: function () { $http.get("/loadQuestions") }
-        }
-    });
