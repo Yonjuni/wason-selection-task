b9365d062412f2df8295a3621e432e23ee5cf2db
Karola Marky <karola@pinyto.de>
2014-11-05 00:01:08 +0900

    basic initial data structure set

---
+++ selectionTask/management/commands/initial_data.py
@@ -0,0 +1,17 @@
+import django
+from django.core.management import BaseCommand
+from django.db import transaction
+
+
+@transaction.atomic
+def create_initial_data():
+    pass
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Creates initial task data in the database'
+
+    def handle(self, *args, **options):
+        self.stdout.write("Creating initial data ...\n")
+        create_initial_data()
