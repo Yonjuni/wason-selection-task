3a402e2c5836b01161bb81392beb1d1de2d57ca2
Karola Marky <karola@pinyto.de>
2014-11-04 18:24:49 +0900

    Japanese translation added

--- static/i18n/resources-locale_jp.js
+++ static/i18n/resources-locale_jp.js
@@ -2,6 +2,51 @@
     {
         "key"        : "_nextButton_",
         "value"      : "次へ",
-        "description": ""
+        "description": "Next button"
+    },
+    {
+        "key"        : "_submitButton_",
+        "value"      : "送信",
+        "description": "Submit button used after the tasks to submit results"
+    },
+    {
+        "key"        : "_taskQuestion_",
+        "value"      : "上記のルールが守られているかどうかを確かめるためには、どのカードをめくって確かめる必要があると思いますか？",
+        "description": "Question regarding the task"
+    },
+    {
+        "key"        : "_welcomeMessage_",
+        "value"      : "実験ページへようこそ",
+        "description": "Welcome Message on introduction page"
+    },
+    {
+        "key"        : "_continueButton_",
+        "value"      : "実験へ進む",
+        "description": "Continue button on introduction page"
+    },
+    {
+        "key"        : "_thanksText_",
+        "value"      : "ご回答ありがとうございました。送信ボタンを押下すると調査サイトへ戻ります",
+        "description": "Thanks text after completing experiment"
+    },
+    {
+        "key"        : "_storyHeading_",
+        "value"      : "ストーリー：",
+        "description": "Heading which says story"
+    },
+    {
+        "key"        : "_ruleHeading_",
+        "value"      : "ルール：",
+        "description": "Heading which says rule"
+    },
+    {
+        "key"        : "_error_",
+        "value"      : "エラー：",
+        "description": "Heading which says error"
+    },
+    {
+        "key"        : "_questionHeading_",
+        "value"      : "質問：",
+        "description": " question"
     }
 ]
